8b805280b209fab605974971048f0ac0050f25ef
Prince-Davy <princedavy.nkakou@gmail.com>
2023-02-21 13:50:21 +0100

    Structuring  Applications

--- src/main/java/com/springboot/masterclass/Application.java
+++ src/main/java/com/springboot/masterclass/Application.java
@@ -7,17 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @GetMapping("test")
-    Customer getCustomer() {
-        return new Customer(1L, "Anny");
     }
 
     record Customer(Long id, String name) {
---
+++ src/main/java/com/springboot/masterclass/customer/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.springboot.masterclass.customer;
+
+public class CustomerRepository {
+}
